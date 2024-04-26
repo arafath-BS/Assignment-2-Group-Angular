a13ae96a78c90fb09223735b242c26edd9a61484
arafath-BS <arafath.santo@brainstation-23.com>
2024-04-26 14:13:57 +0600

    multiplication code added to the feature branch by santo

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,5 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+
+int multiplication(int firstNumber, int secondNumber)
+{
+   return firstNumber*secondNumber;
+}
+
+
 int main(){
+
+    int firstNumber,secondNumber;
+    char operand;
+
+   
+
     
+    cout<<"Enter first number\n";
+    cin>>firstNumber;
+
+    cout<<"Enter Second number\n";
+    cin>>secondNumber;
+
+
+    cout<<"Enter Operand\n";
+    cin>>operand;
+
+
+ 
+        
+        switch (operand){
+
+            case '+':
+            cout<<multiplication(firstNumber,secondNumber)<<endl;
+            break;
+
+            case '-':
+            cout<<multiplication(firstNumber,secondNumber)<<endl;
+            break;
+
+            case '*':
+            cout<<multiplication(firstNumber,secondNumber)<<endl;
+            break;
+
+            case '/':
+            cout<<multiplication(firstNumber,secondNumber)<<endl;
+            break;
+
+        }
+
+     
+    
+    return 0;
 }
