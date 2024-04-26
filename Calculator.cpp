817c9ee8064604b011caedc40f876403c0acd241
asifmahmud35 <asif.mahmud@brainstation-23.com>
2024-04-26 14:15:48 +0600

    Implement division feature

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,5 +1,11 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-    
+
+double divide(double num1, double num2);
+
+
+double divide(double num1, double num2) {
+    if (num2 == 0) {
+        std::cout << "Error: Division by zero is not allowed." << std::endl;
+        return 0; 
+    }
+    return num1 / num2;
 }
